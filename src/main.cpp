a1ca8f72938662e34005869f4ed848b71212de23
Mark 5 <markvudacity@gmail.com>
2018-01-08 12:25:17 -0500

    [chore] Cleaning up main a bit.

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,13 @@
 // for convenience
 using json = nlohmann::json;
 
-// For converting back and forth between radians and degrees.
-constexpr double pi() { return M_PI; }
+static bool justSwitchedToManual = true; // Just helpful printing when switching to manual
 
-double deg2rad(double x) { return x * pi() / 180; }
+static const int WEBSOCKECT_OK_DISCONNECT_CODE = 1000;
 
-double rad2deg(double x) { return x * 180 / pi(); }
+static const std::string RESET_SIMULATOR_WS_MESSAGE = "42[\"reset\", {}]";
+
+static const std::string MANUAL_WS_MESSAGE = "42[\"manual\",{}]";
 
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
@@ -42,12 +43,18 @@ int main() {
         // "42" at the start of the message means there's a websocket message event.
         // The 4 signifies a websocket message
         // The 2 signifies a websocket event
+
         if (length && length > 2 && data[0] == '4' && data[1] == '2') {
             auto s = hasData(std::string(data).substr(0, length));
             if (s != "") {
                 auto j = json::parse(s);
                 std::string event = j[0].get<std::string>();
                 if (event == "telemetry") {
+                    if (!justSwitchedToManual) {
+                        std::cout << "Taking back control from manual!!" << std::endl;
+                        justSwitchedToManual = true;
+                    }
+
                     // j[1] is the data JSON object
                     const double cte = std::stod(j[1]["cte"].get<std::string>());
                     const double speed = std::stod(j[1]["speed"].get<std::string>());
@@ -56,26 +63,26 @@ int main() {
                     pid.UpdateError(cte);
 
                     double steer_value = pid.SteerValue();
-
-                    /*
-                    * TODO: Calculate steering value here, remember the steering value is [-1, 1].
-                    * NOTE: Feel free to play around with the throttle and speed. Maybe use
-                    * another PID controller to control the speed!
-                    */
-
                     // DEBUG
-                    std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
+                    //std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
                     json msgJson;
                     msgJson["steering_angle"] = steer_value;
                     msgJson["throttle"] = 0.3;
+
                     auto msg = "42[\"steer\"," + msgJson.dump() + "]";
                     std::cout << msg << std::endl;
                     ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+                } else {
+                    std::cout << "What events do we receive that is non-telemetry? " << event << std::endl;
                 }
             } else {
-                // Manual driving
-                std::string msg = "42[\"manual\",{}]";
+                if (justSwitchedToManual) {
+                    justSwitchedToManual = false;
+                    std::cout << "Switched to manual mode!!" << std::endl;
+                }
+
+                auto msg = MANUAL_WS_MESSAGE;
                 ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
             }
         }
@@ -97,9 +104,57 @@ int main() {
         std::cout << "Connected!!!" << std::endl;
     });
 
+    h.onError([](void *user) {
+        // Code copied from: https://github.com/uNetworking/uWebSockets/blob/master/tests/main.cpp
+        switch ((long) user) {
+            case 1:
+                std::cout << "Client emitted error on invalid URI" << std::endl;
+                break;
+            case 2:
+                std::cout << "Client emitted error on resolve failure" << std::endl;
+                break;
+            case 3:
+                std::cout << "Client emitted error on connection timeout (non-SSL)" << std::endl;
+                break;
+            case 5:
+                std::cout << "Client emitted error on connection timeout (SSL)" << std::endl;
+                break;
+            case 6:
+                std::cout << "Client emitted error on HTTP response without upgrade (non-SSL)" << std::endl;
+                break;
+            case 7:
+                std::cout << "Client emitted error on HTTP response without upgrade (SSL)" << std::endl;
+                break;
+            case 10:
+                std::cout << "Client emitted error on poll error" << std::endl;
+                break;
+            case 11:
+                static int protocolErrorCount = 0;
+                protocolErrorCount++;
+                std::cout << "Client emitted error on invalid protocol" << std::endl;
+                if (protocolErrorCount > 1) {
+                    std::cout << "FAILURE:  " << protocolErrorCount << " errors emitted for one connection!" << std::endl;
+                    //exit(-1);
+                }
+                break;
+            default:
+                std::cout << "FAILURE: " << user << " should not emit error!" << std::endl;
+                //exit(-1);
+        }
+    });
+
     h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code, char *message, size_t length) {
-        ws.close();
-        std::cout << "Disconnected" << std::endl;
+        if (code == WEBSOCKECT_OK_DISCONNECT_CODE) {
+            std::cout << "Disconnected normally" << std::endl;
+        } else {
+            std::cout << "Unexpected Disconnect with code: " << code << std::endl;
+        }
+
+        std::cout << "WARN: Not closing WS because we get bad access exception! (Which one cannot catch in C++!?)"  << std::endl;
+        // StackOverflow https://stackoverflow.com/q/19304157 suggested that error code 1006 means to check onError
+        // But, but, but, adding onError here doesn't get called at all, everythign seems alright
+        // (other than this ws.close() exc_bad_access)
+        //ws.close(code, message, length); // Besides, if we're getting disconnection method, then the connection is already closed??
     });
 
     int port = 4567;
