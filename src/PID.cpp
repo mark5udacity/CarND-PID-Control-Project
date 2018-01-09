d5944703fb8f422366704f84cae07e12ca79b251
Mark 5 <markvudacity@gmail.com>
2018-01-08 22:37:53 -0500

    [PID] Futz around more with I-error, using a smoothing value to reduce it's sum by 10% each time.

--- src/PID.cpp
+++ src/PID.cpp
@@ -26,11 +26,12 @@ void PID::Init(double Kp, double Ki, double Kd) {
 
 void PID::UpdateError(const double cte) {
     i_error += cte * .1;
+    i_error *= .90; // Reduce by 10 percent!  Just like from Twiddle code
 
-    // TODO: Consider Removing this  Alpha smoothing-- give stronger bias to more recent errors over older ones
-    if (abs(i_error) > 10.) {
-        i_error = 0.; // This is one way, alternative is to keep list of X-most recent points.
-    }
+    // TODO: Consider removing/tweaking this^^  Alpha smoothing, which gives stronger bias to more recent errors over older ones
+    //if (abs(i_error) > 10.) {
+    //        i_error = 0.; // This is one way, alternative is to keep list of X-most recent points.
+    //}
 
     double diff_cte;
     if (d_error == INFINITY) {
